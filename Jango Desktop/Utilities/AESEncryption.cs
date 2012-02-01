2df8bd59560fda0010f834bda7a98ad95dfc6a79
SamPlusPlus <Sam@SamPlusPlus.com>
2012-02-01 00:13:52 -0500

    Added correct license file.

--- Jango Desktop/Utilities/AESEncryption.cs
+++ Jango Desktop/Utilities/AESEncryption.cs
@@ -1,4 +1,26 @@
-﻿using System;
+﻿/*
+ * Copyright (c) 2010 <a href="http://www.gutgames.com">James Craig</a>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Security.Cryptography;
@@ -6,7 +28,7 @@ using System.IO;
 
 namespace Jango_Desktop.Utilities
 {
-     /// <summary>
+    /// <summary>
     /// Utility class that handles encryption
     /// Code from http://www.gutgames.com/post/AES-Encryption-in-C.aspx
     /// </summary>
@@ -26,9 +48,9 @@ namespace Jango_Desktop.Utilities
         /// <param name="KeySize">Can be 128, 192, or 256</param>
         /// <returns>An encrypted string</returns>
         public static string Encrypt(string PlainText, string Password,
-            string Salt, string HashAlgorithm,
-            int PasswordIterations, string InitialVector,
-            int KeySize)
+                                     string Salt, string HashAlgorithm,
+                                     int PasswordIterations, string InitialVector,
+                                     int KeySize)
         {
             try
             {
@@ -74,9 +96,9 @@ namespace Jango_Desktop.Utilities
         /// <param name="KeySize">Can be 128, 192, or 256</param>
         /// <returns>A decrypted string</returns>
         public static string Decrypt(string CipherText, string Password,
-            string Salt, string HashAlgorithm,
-            int PasswordIterations, string InitialVector,
-            int KeySize)
+                                     string Salt, string HashAlgorithm,
+                                     int PasswordIterations, string InitialVector,
+                                     int KeySize)
         {
             try
             {
